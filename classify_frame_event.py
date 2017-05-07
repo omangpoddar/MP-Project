ec012ef747ad88e58f0f7a9982a1d694da421a55
NK1830 <nitesh.kataria@iiitb.org>
2017-05-07 21:56:52 +0530

    Update classify_frame_event.py

--- classify_frame_event.py
+++ classify_frame_event.py
@@ -32,17 +32,6 @@ from sklearn.model_selection import GridSearchCV
 counter=0
 filename_list=[]
 
-def isSubSequence(string1, string2, m, n):
-    # Base Cases
-    if m == 0:    return True
-    if n == 0:    return False
- 
-    # If last characters of two strings are matching
-    if string1[m-1] == string2[n-1]:
-        return isSubSequence(string1, string2, m-1, n-1)
- 
-    # If last characters are not matching
-    return isSubSequence(string1, string2, m, n-1)
 
 def EPR(img):
 	edges = cv2.Canny(img,100,200)
